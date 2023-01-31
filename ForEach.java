2bf6488cbfabaadb2fb663e2f61192de99f52ab5
issach315 <117745188+issach315@users.noreply.github.com>
2023-01-31 12:34:46 +0530

    for each loop syntax adding

--- ForEach.java
+++ ForEach.java
@@ -9,12 +9,22 @@ public class ForEach
 
     int x[] = {1,2,3,4,5,6,7,8,9};
 
-    for (int a : x)
+    for (int a :x)
      {
          System.out.println(a);
  
         
     }
+    
+   /* 
+   
+   for(dataType variableName : array)
+    {
+     
+     System.out.println(variableName);
+    }
+    
+    */
         
     
    }
