c64c8a7891b82df90600a0fd766dec75dd61b310
issach315 <issachemani227@gmial.com>
2023-01-31 12:30:57 +0530

    for-each loop in java

---
+++ ForEach.java
@@ -0,0 +1,22 @@
+package For_eachLoop;
+
+public class ForEach
+ {
+
+   public static void main(String[] args)
+    
+   {
+
+    int x[] = {1,2,3,4,5,6,7,8,9};
+
+    for (int a : x)
+     {
+         System.out.println(a);
+ 
+        
+    }
+        
+    
+   }
+    
+}
